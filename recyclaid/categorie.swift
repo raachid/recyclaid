bddf4b311398b4c2527aeaf0b49cfde82c399bf0
joo-jo <johan--d@live.fr>
2020-05-11 10:12:27 +0200

    categorie

---
+++ recyclaid/categorie.swift
@@ -0,0 +1,22 @@
+//
+//  categorie.swift
+//  recyclaid
+//
+//  Created by johanD on 11/05/2020.
+//  Copyright © 2020 Kerbach Rachid. All rights reserved.
+//
+
+import SwiftUI
+
+struct categorie: View {
+    var body: some View {
+        NavigationView {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+}
+struct categorie_Previews: PreviewProvider {
+    static var previews: some View {
+        categorie()
+    }
+}
