1bad47acd22e9925d48fe0250e6f1999797a9490
joo-jo <johan--d@live.fr>
2020-05-13 11:34:24 +0200

    casse tout test

---
+++ recyclaid/cassetout.swift
@@ -0,0 +1,21 @@
+//
+//  cassetout.swift
+//  recyclaid
+//
+//  Created by johanD on 13/05/2020.
+//  Copyright © 2020 Kerbach Rachid. All rights reserved.
+//
+
+import SwiftUI
+
+struct cassetout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct cassetout_Previews: PreviewProvider {
+    static var previews: some View {
+        cassetout()
+    }
+}
